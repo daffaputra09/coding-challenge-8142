191b1214a99cccfda8c0b66ac308b02b87b78b2c
daffaputra09 <daffaputra863@gmail.com>
2023-10-07 23:44:32 +0700

    second commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,21 @@
+import java.text.DecimalFormat
+
 fun main(args: Array<String>) {
     while (true){
         print("Masukkan angka: ")
 
         val input_number = readln()
         try {
-            val number = input_number.toInt()
+            val number = input_number.toLong()
 
             for (i in 1..number){
                 val cubic = i*i*i
-                println("Current Number is: $i and the cube is $cubic")
+                if(cubic < Long.MAX_VALUE) println("Current Number is: $i and the cube is $cubic")
+                else println("Hasil cubic lebih dari ${Long.MAX_VALUE}")
             }
         }
         catch (e: Exception){
-            println("HANYA BISA MEMASUKKAN ANGKA")
+            println("HANYA BISA MEMASUKKAN ANGKA DAN LEBIH DARI 0")
         }
     }
 }
